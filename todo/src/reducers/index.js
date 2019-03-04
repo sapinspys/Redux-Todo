d829989d5fd10c58b0fe42a08c9b22eb0f9923bd
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-04 18:05:31 -0500

    Begun splitting reducers for readability

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -7,6 +7,26 @@ const initialState = {
 
 // REDUCER HANDLES LOGIC DICTATED BY ACTIONS
 
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO:
+            return [...state, {
+                text: action.text,
+                completed: false
+            }]
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                } return todo
+            })
+        default:
+            return state
+    }
+}
+
 function todoApp(state = initialState, action) {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
