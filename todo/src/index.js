f355baa6b41886ac9500100ff3789faf66a36581
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-04 17:26:20 -0500

    Created Redux store without rootReducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+const store = createStore()
 
 ReactDOM.render(
     <Provider>
