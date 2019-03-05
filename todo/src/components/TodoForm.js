363ae28c82ab520020cd3e00d82580b60dd2102f
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-05 14:55:50 -0500

    Styled TodoForm

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -6,50 +6,50 @@ import { connect } from "react-redux";
 import { addTodo } from "../actions";
 
 class TodoForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todo: "" 
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+				todo: "" 
+		}
+	}
 
-    handleSubmit = e => {
-        e.preventDefault();
-        
-        // Calling Action Creator:
-        this.props.addTodo(this.state.todo) 
+	handleSubmit = e => {
+		e.preventDefault();
+		
+		// Calling Action Creator:
+		this.props.addTodo(this.state.todo) 
 
-        this.setState({
-            todo: "" 
-        })
-    }
+		this.setState({
+				todo: "" 
+		})
+	}
 
-    handleChange = e => {
-        this.setState({
-            todo: e.target.value
-        })
-    }
+	handleChange = e => {
+		this.setState({
+				todo: e.target.value
+		})
+	}
 
-    render () {
-        return (
-            <form className="todoForm"
-                onSubmit={this.handleSubmit} 
-                autoComplete="off">
-                <input type="text"
-                    name="todo"
-                    value={this.state.todo}
-                    onChange={this.handleChange}
-                    placeholder="...Todo"/>
-                <button>Add Todo</button>
-            </form>
-        )
-    }
+	render () {
+		return (
+				<form className="todoForm"
+						onSubmit={this.handleSubmit} 
+						autoComplete="off">
+						<input type="text"
+								name="todo"
+								value={this.state.todo}
+								onChange={this.handleChange}
+								placeholder="...Todo"/>
+						<button>Add Todo</button>
+				</form>
+		)
+	}
 }
 
-const mapStateToProps = (state) => {
-    return {
-        todos: state.todos
-    }
+	const mapStateToProps = (state) => {
+	return {
+		todos: state.todos
+	}
 }
 
 export default connect(mapStateToProps, { addTodo })(TodoForm);
